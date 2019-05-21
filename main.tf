5a4033f9feea297c49b891a17b615bd4e2baa1cd
Mariano Paz <paz.mariano@gmail.com>
2019-05-21 13:43:34 -0300

    Outbound all

--- main.tf
+++ main.tf
@@ -77,8 +77,8 @@ resource "aws_security_group" "bastion_host_security_group" {
   }
 
   egress {
-    from_port   = -1
-    to_port     = -1
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
