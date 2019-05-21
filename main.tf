d218542dad24d501efa5b16c1d07f1c4102570a5
Mariano Paz <paz.mariano@gmail.com>
2019-05-20 23:47:52 -0300

    Default allow egress all to internet

--- main.tf
+++ main.tf
@@ -76,6 +76,13 @@ resource "aws_security_group" "bastion_host_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = "${merge(var.tags)}"
 }
 
